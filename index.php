f5470c8a0c22c3db81a857777526416d5b468a1b
krz-moskalik <krz-moskalik@gmail.com>
2013-11-14 21:05:19 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta content="text/html" charset="utf-8">
+        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+        <script src="/js/carousel.js" type="text/javascript"></script>
+        <link href="" rel="stylesheet">
+    </head>
+    <body>
+
+    </body>
+</html>
