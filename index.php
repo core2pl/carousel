a788f50c55dcc441751351b35a258e7480971700
krz-moskalik <krz-moskalik@gmail.com>
2013-11-18 08:17:59 +0100

    first almost working version

--- index.php
+++ index.php
@@ -4,9 +4,24 @@
         <meta content="text/html" charset="utf-8">
         <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
         <script src="/js/carousel.js" type="text/javascript"></script>
-        <link href="" rel="stylesheet">
+        <link href="/css/style.css" rel="stylesheet" type="text/css">
+        <script type="text/javascript">
+            /*var carousel = {
+                animationTime: 500,
+                fadeTime: 5000,
+                containerClass: 'carousel',
+                stopOnHover: true,
+                images: [
+                    '/img/img1.jpg',
+                    '/img/img2.jpg'
+                ]
+            }*/
+            function log(c) {
+                console.log(c);
+            }
+        </script>
     </head>
     <body>
-
+        <div class="carousel"></div>
     </body>
 </html>
--- js/carousel.js
+++ js/carousel.js
@@ -1,1 +1,40 @@
+var carousel = {
+    animationTime: 1000,
+    fadeTime: 2000,
+    containerClass: 'carousel',
+    stopOnHover: true,
+    currentImage: 0,
+    images: [
+        '/img/img1.jpg',
+        '/img/img2.jpg'
+    ]
+}
 
+function carouselAnim() {
+    $('.'+carousel.containerClass+'_slider').animate(
+        { 'left': '+=150'},
+        {
+            duration: carousel.animationTime,
+            complete: function () {
+                $('.'+carousel.containerClass+'_image:last').prependTo('.'+carousel.containerClass+'_slider');
+                $('.'+carousel.containerClass+'_slider').css('right', 0);
+            }
+        }
+    );
+
+}
+
+$(document).ready(
+    function () {
+        $('<div/>', {
+            class: carousel.containerClass+'_slider'
+        }).css('width', carousel.images.length*$('.' +carousel.containerClass).innerWidth()).appendTo($('.'+carousel.containerClass));
+        for (var i = 0; i < carousel.images.length; i++) {
+            $('<div/>', {
+                class: carousel.containerClass+'_image'
+            }).css('background-image', 'url('+carousel.images[i]+')').appendTo($('.'+carousel.containerClass+'_slider'));
+
+        }
+        setInterval(function () { carouselAnim() }, carousel.fadeTime);
+    }
+);
